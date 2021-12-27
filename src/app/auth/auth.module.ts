f80c169c4987b63d4e32d10d6ba6f459e5a1c6a1
ArtemIvanov <Ivanov.Ar.V@sblogistica.ru>
2021-12-27 13:42:30 +0300

    Create reducers form auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule } from '@angular/forms'
 import { StoreModule } from '@ngrx/store'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { environment } from '../../environments/environment'
 
 import { AuthRoutingModule } from './auth-routing.module'
 import { RegisterComponent } from './components/register/register.component'
+import { reducers } from './store/reducers'
 
 @NgModule({
   declarations: [RegisterComponent],
@@ -14,11 +13,7 @@ import { RegisterComponent } from './components/register/register.component'
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production,
-    }),
+    StoreModule.forFeature('auth', reducers),
   ],
 })
 export class AuthModule {}
