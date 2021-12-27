a8816037951fe3edabfb44ff468ceac3791e0bb4
ArtemIvanov <Ivanov.Ar.V@sblogistica.ru>
2021-12-27 13:28:49 +0300

    Install NgRx

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { environment } from '../../environments/environment'
 
 import { AuthRoutingModule } from './auth-routing.module'
 import { RegisterComponent } from './components/register/register.component'
-import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
 })
 export class AuthModule {}
---
+++ src/app/auth/store/actionTypes.ts
@@ -0,0 +1,5 @@
+export const enum ActionTypes {
+  REGISTER = '[Auth] Register',
+  REGISTER_SUCCESS = '[Auth] Register success',
+  REGISTER_FAILURE = '[Auth] Register failure',
+}
